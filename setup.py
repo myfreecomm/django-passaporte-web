f9d78ca874384e3e5962722b3c4a573cf2f2be45
Vitor M. A. da Cruz <vitor.mazzi@myfreecomm.com.br>
2014-10-03 15:03:16 -0300

    Version bump

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ class Tox(TestCommand):
 
 setuptools.setup(
     name="django-passaporte-web",
-    version="1.1.7",
+    version="1.2",
     packages=["identity_client"],
     include_package_data=True,  # declarations in MANIFEST.in
     install_requires=open(join(dirname(__file__), 'requirements.txt')).readlines(),
